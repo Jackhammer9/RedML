793cc2117856798b8a7876990a319e9be42e44dd
Arnav Bajaj <49639514+JackhammerYT@users.noreply.github.com>
2025-01-09 17:08:56 +0100

    Fix setup.py and pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RedML',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -14,10 +14,16 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Jackhammer9/RedML',
+    project_urls={
+        "Bug Tracker": "https://github.com/JackhammerYT/RedML/issues",
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=['numpy' , 'matplotlib'],
     python_requires='>=3.6',
 )
